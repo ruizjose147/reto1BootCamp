79a0f4368fbb781462ab663f7f6a356232c189e4
JoseRuiz <ruizjose_147@hotmail.com>
2022-02-09 08:55:42 -0500

    tres primeras lineas =)

---
+++ main.js
@@ -0,0 +1,9 @@
+let fila = 10;
+let columna = 10;
+let numerales = parseInt((m*n)/10);
+var matriz[fila][columna];
+for(var i = 0;i < fila; i++){
+    for(var j = 0; j < columna; j++){
+        matriz[i][j]=
+    }
+}
